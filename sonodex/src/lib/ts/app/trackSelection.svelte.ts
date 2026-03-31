f805c962af90e0cfe89ffa68af043c2c16da1474
PsychicSnail <71180389+PsychicSnailStudios@users.noreply.github.com>
2026-03-30 19:33:43 -0500

    fix copy name and artist

--- sonodex/src/lib/ts/app/trackSelection.svelte.ts
+++ sonodex/src/lib/ts/app/trackSelection.svelte.ts
@@ -99,5 +99,5 @@ export function copySelectedUIDsToClipboard(orderedUids: string[]) {
 	writeText(uids).catch(() => {});
 }
 export function copySelectedNameToClipboard(track: Track) {
-	writeText(track.title ?? "Unknown Title" + "; " + (track.album_artist ?? "Unknown Artist")).catch(() => {});
+	writeText((track.title ?? "Unknown Title") + "; " + (track.album_artist ?? "Unknown Artist")).catch(() => {});
 }
