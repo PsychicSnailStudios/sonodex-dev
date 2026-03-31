722ad8fcc90a69147c730effbf2c89ce46611e85
PsychicSnail <71180389+PsychicSnailStudios@users.noreply.github.com>
2026-03-30 19:58:19 -0500

    fix missing import

--- sonodex/src/lib/ts/session.svelte.ts
+++ sonodex/src/lib/ts/session.svelte.ts
@@ -2,6 +2,7 @@ import { SortState } from "$lib/ts/app/sortConfig.svelte";
 import { createColumnState } from "$lib/ts/app/columnConfig.svelte";
 import type { SortField, SortDirection } from "$lib/ts/app/sortConfig.svelte";
 import type { ColumnKey } from "$lib/ts/app/columnConfig.svelte";
+import { clamp } from "$lib/ts/util/helpers";
 
 type Selection = {
 	uid: string;
