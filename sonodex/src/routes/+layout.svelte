dd0f924b6867851f3a037e629b6e84ced3082904
PsychicSnail <71180389+PsychicSnailStudios@users.noreply.github.com>
2026-04-16 16:48:21 -0500

    Update +layout.svelte

--- sonodex/src/routes/+layout.svelte
+++ sonodex/src/routes/+layout.svelte
@@ -45,8 +45,8 @@
 
 <style>
   .main-app-bg {
-    //background-color: oklch(from var(--background) l c h / 95%);
-	radial-gradient(circle,oklch(from var(--background) l c h / 95%) 0%, oklch(from var(--background) l c h / 99%) 100%);
+    /* background-color: oklch(from var(--background) l c h / 95%); */
+    background: radial-gradient(circle, oklch(from var(--background) l c h / 99%) 0%, oklch(from var(--background) l c h / 96%) 100%);
     backdrop-filter: blur(100px);
   }
 </style>
